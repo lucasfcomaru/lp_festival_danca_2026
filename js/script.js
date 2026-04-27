e673b815fac606c9f525a6be4260f76223316a6b
lucasfcomaru <lukscomaru@gmail.com>
2026-04-27 15:56:07 -0300

    index.html

---
+++ js/script.js
@@ -0,0 +1,26 @@
+ // JavaScript para o menu mobile
+        const menuToggle = document.querySelector('.menu-toggle');
+        const navMenu = document.querySelector('.nav-menu');
+        const overlay = document.querySelector('.overlay');
+
+        menuToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+            overlay.classList.toggle('active');
+
+        });
+
+        // Fechar o menu ao clicar no overlay
+        overlay.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+            overlay.classList.remove('active');
+        });
+
+        // Fechar o menu ao clicar em um link (âncora)
+        const menuLinks = document.querySelectorAll('.nav-menu a');
+
+        menuLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                navMenu.classList.remove('active');
+                overlay.classList.remove('active');
+            });
+        });
