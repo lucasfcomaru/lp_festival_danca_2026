b6fdebdd1e0f62e6d785fb4b60a5a3e78114342e
lucasfcomaru <lukscomaru@gmail.com>
2026-04-28 16:03:23 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
- // JavaScript para o menu mobile
-        const menuToggle = document.querySelector('.menu-toggle');
-        const navMenu = document.querySelector('.nav-menu');
-        const overlay = document.querySelector('.overlay');
+// JavaScript para o menu mobile
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const overlay = document.querySelector(".overlay");
 
-        menuToggle.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
-            overlay.classList.toggle('active');
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
 
-        });
+// Fechar o menu ao clicar no overlay
+overlay.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
 
-        // Fechar o menu ao clicar no overlay
-        overlay.addEventListener('click', () => {
-            navMenu.classList.remove('active');
-            overlay.classList.remove('active');
-        });
+// Fechar o menu ao clicar em um link (âncora)
+const menuLinks = document.querySelectorAll(".nav-menu a");
 
-        // Fechar o menu ao clicar em um link (âncora)
-        const menuLinks = document.querySelectorAll('.nav-menu a');
-
-        menuLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                navMenu.classList.remove('active');
-                overlay.classList.remove('active');
-            });
-        });
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
